844e10cde91b179b4bceaf086d94f18d75d7a3ad
Jakob Voss <voss@gbv.de>
2023-09-07 10:26:45 +0200

    Minor layout changes

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 
-$conf = parse_ini_file("configuration.cnf", false, INI_SCANNER_TYPED);
-$userQuery = isset($_GET['query']) && !empty($_GET['query']) ? $_GET['query'] : '';
-$type = isset($_GET['type']) && in_array($_GET['type'], ['map', 'timeline']) ? $_GET['type'] : 'map';
+if (file_exists("configuration.cnf")) {
+  $conf = parse_ini_file("configuration.cnf", false, INI_SCANNER_TYPED);
+} else {
+  die("Missing configuration.cnf!");
+}
+
+$query = @$_GET['query'] ?: '';
+$type = in_array(@$_GET['type'], ['map', 'timeline']) ? $_GET['type'] : 'map';
 
 ?><!DOCTYPE html>
 <html>
@@ -10,22 +15,25 @@ $type = isset($_GET['type']) && in_array($_GET['type'], ['map', 'timeline']) ? $
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $conf['catalogue_name'] ?> holdings visualisation</title>
+  <title><?= @$conf['catalogue_name'] ?> holdings visualisation</title>
+  <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
   <script src="https://d3js.org/d3.v6.min.js"></script>
   <script src="https://kit.fontawesome.com/2f4e00a49c.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="geo.css" />
 </head>
 <body>
   <header>
-    <button id="zoom-in">+</button>
-    <button id="zoom-out">-</button>
-    <span class="title"><a href="<?= $conf['catalogue_url'] ?>" target="_blank"><?= $conf['catalogue_name'] ?></a> holdings visualisation</span>
+    <h1>
+      <a href="<?= @$conf['catalogue_url'] ?>" target="_blank"><?= @$conf['catalogue_name'] ?></a> holdings visualisation
+    </h1>
     <form style="display: inline;">
-      <input type="text" name="query" id="search" value="<?= $userQuery ?>">
+      <input type="text" name="query" id="search" value="<?= $query ?>">
       <input type="submit" value="search catalogue!" id="submit">
       <input type="radio" name="type" value="map" id="type-map" <?php if ($type == 'map') { ?>checked="checked"<?php } ?>><label for="type-map">map</label>
       <input type="radio" name="type" value="timeline" id="type-timeline" <?php if ($type == 'timeline') { ?>checked="checked"<?php } ?>><label for="type-timeline">timeline</label>
     </form>
+    <button id="zoom-in">+</button>
+    <button id="zoom-out">-</button>
   </header>
 
   <div id="timeline-container"></div>
@@ -43,7 +51,7 @@ $type = isset($_GET['type']) && in_array($_GET['type'], ['map', 'timeline']) ? $
       mapCreated: false,
       timelineCreated: false,
       qaCatalogueBaseURL: '<?= $conf['qa_catalogue_base_url'] ?>'
-    };
+    }
   </script>
   <script type="module" src="index.js"></script>
 </body>
